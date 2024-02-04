45db842423473f36dfdd1d90f36803e8f47f709a
Igor <130784433+xigor1337@users.noreply.github.com>
2024-02-04 15:41:55 +0100

    Animations in home

--- src/home.ts
+++ src/home.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
