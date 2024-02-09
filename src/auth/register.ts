da9acdba29d954a07b7bf3456a0e9dc48c4a1857
Bartek <bmroczkowski@icloud.com>
2024-02-09 22:18:12 +0100

    Working on register page. Add topbar website icon.

--- src/auth/register.ts
+++ src/auth/register.ts
@@ -1,1 +1,15 @@
-console.log("Hello Register")
+document.getElementById("submit-btn")?.addEventListener("click", async (e) => {
+    e.preventDefault()
+
+    await fetch("/api/auth/register/", {
+        method: "POST",
+        headers: { "X-CSRFToken": (document.getElementsByName("csrfmiddlewaretoken")[0] as HTMLInputElement).value },
+        body: JSON.stringify({
+            "hello": "world!",
+        }),
+    }).then(() => {
+
+    }).catch(() => {
+
+    })
+})
