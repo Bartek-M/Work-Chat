3b4f39edd7f277cc9592a6af98d6c480d549ddf2
Igor <130784433+xigor1337@users.noreply.github.com>
2024-02-09 23:41:44 +0100

    Working on high contrast mode

--- src/home.ts
+++ src/home.ts
@@ -1,14 +1,19 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        console.log(entry);
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            if (entry.target.classList.contains('hidden')) {
+                entry.target.classList.add('show');
+            } else if (entry.target.classList.contains('hidden-line')) {
+                entry.target.classList.add('show-line');
+            }
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.hidden, .hidden-line');
 hiddenElements.forEach((el) => observer.observe(el));
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.getElementById('toggleContrastBtn');
@@ -24,8 +29,27 @@ function toggleContrast(): void {
         const currentTheme = mainElement.getAttribute('data-theme');
         if (currentTheme === 'high-contrast') {
             mainElement.setAttribute('data-theme', '');
+
+            document.querySelectorAll('[data-original-class]').forEach((el: Element) => {
+                const originalClass = el.getAttribute('data-original-class');
+                if (originalClass) {
+                    el.classList.remove('text-contrast');
+                    el.classList.add(originalClass);
+                    el.removeAttribute('data-original-class');
+                }
+            });
         } else {
             mainElement.setAttribute('data-theme', 'high-contrast');
+
+            //list of all overwriten classes
+            const classList = ['text-danger', 'text-success', 'text-success-emphasis', 'text-body-secondary'];
+            classList.forEach(cls => {
+                document.querySelectorAll('.' + cls).forEach((el: Element) => {
+                    el.setAttribute('data-original-class', cls);
+                    el.classList.remove(cls);
+                    el.classList.add('text-contrast');
+                });
+            });
         }
     } else {
         console.error('Main element not found');
