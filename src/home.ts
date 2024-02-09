c78cb939d2c2485deea9f6fed8c005f93904768c
Igor <130784433+xigor1337@users.noreply.github.com>
2024-02-09 22:45:09 +0100

    Working on home page. Creation of high contrast mode

--- src/home.ts
+++ src/home.ts
@@ -9,3 +9,25 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggleContrastBtn');
+    if (toggleBtn) {
+        toggleBtn.addEventListener('click', toggleContrast);
+    }
+});
+
+
+function toggleContrast(): void {
+    const mainElement = document.getElementById('main-object');
+    if (mainElement) {
+        const currentTheme = mainElement.getAttribute('data-theme');
+        if (currentTheme === 'high-contrast') {
+            mainElement.setAttribute('data-theme', '');
+        } else {
+            mainElement.setAttribute('data-theme', 'high-contrast');
+        }
+    } else {
+        console.error('Main element not found');
+    }
+}
