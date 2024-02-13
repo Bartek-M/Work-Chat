103ebe65cf41351c643e6bc9d316ff643a3c46e6
Bartek <bmroczkowski@icloud.com>
2024-02-13 22:05:18 +0100

    Start working on chat/ page

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,4 @@
 console.log("Hello App")
+
+document.getElementById("chat-open")?.addEventListener("click", e => document.getElementById("chat-wrapper")?.classList.add("active"))
+document.getElementById("chat-close")?.addEventListener("click", e => document.getElementById("chat-wrapper")?.classList.remove("active"))
