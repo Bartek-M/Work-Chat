323d9ee203b5adf69ab88de441ae13f006e45ff0
Bartek <bmroczkowski@icloud.com>
2024-02-14 22:32:05 +0100

    Handle API errors in register/ page. Change danger color. Update font-size in login/ + register/.

--- src/auth/register.ts
+++ src/auth/register.ts
@@ -5,7 +5,6 @@ document.getElementById("register-form")?.addEventListener("submit", async (e) =
 
     let first_name = (document.getElementById("first-name-input") as HTMLInputElement).value
     let last_name = (document.getElementById("last-name-input") as HTMLInputElement).value
-
     let username = (document.getElementById("username-input") as HTMLInputElement).value
     let email = (document.getElementById("email-input") as HTMLInputElement).value
     let password = (document.getElementById("password-input") as HTMLInputElement).value
@@ -14,7 +13,10 @@ document.getElementById("register-form")?.addEventListener("submit", async (e) =
 
     await fetch("/api/auth/register/", {
         method: "POST",
-        headers: { "X-CSRFToken": (document.getElementsByName("csrfmiddlewaretoken")[0] as HTMLInputElement).value },
+        headers: {
+            "Content-type": "application/json",
+            "X-CSRFToken": (document.getElementsByName("csrfmiddlewaretoken")[0] as HTMLInputElement).value
+        },
         body: JSON.stringify({
             first_name: first_name,
             last_name: last_name,
@@ -22,11 +24,20 @@ document.getElementById("register-form")?.addEventListener("submit", async (e) =
             email: email,
             password: password
         }),
-    }).then((resp) => {
-        console.log(resp)
-
+    }).then(async (resp) => {
         if (resp.status == 200) return window.location.replace("/")
-    }).catch(() => {
 
+        await resp.json().then((data) => {
+            let errors = data.errors
+            if (!errors) return // TODO: Show toast message - Something went wrong
+
+            document.getElementById("first-name-error")!.innerText = errors.first_name ? `- ${errors.first_name[0].message}` : "*"
+            document.getElementById("last-name-error")!.innerText = errors.last_name ? `- ${errors.last_name[0].message}` : "*"
+            document.getElementById("email-error")!.innerText = errors.email ? `- ${errors.email[0].message}` : "*"
+            document.getElementById("username-error")!.innerText = errors.username ? `- ${errors.username[0].message}` : "*"
+            document.getElementById("password-error")!.innerText = errors.password ? `- ${errors.password[0].message}` : "*"
+        })
+    }).catch(() => {
+        // TODO: Show toast message - Something went wrong
     })
 })
