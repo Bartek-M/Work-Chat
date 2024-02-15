3ebe8ba88f02fc80bd9e108af2793583d6dcefff
Bartek <bmroczkowski@icloud.com>
2024-02-15 21:26:23 +0100

    Add handling API errors in login/, set inputs as required.

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -10,15 +10,24 @@ document.getElementById("login-form")?.addEventListener("submit", async (e) => {
 
     await fetch("/api/auth/login/", {
         method: "POST",
-        headers: { "X-CSRFToken": (document.getElementsByName("csrfmiddlewaretoken")[0] as HTMLInputElement).value },
+        headers: {
+            "Content-type": "application/json",
+            "X-CSRFToken": (document.getElementsByName("csrfmiddlewaretoken")[0] as HTMLInputElement).value
+        },
         body: JSON.stringify({
             login_data: login_data,
             password: password
         }),
-    }).then((resp) => {
-        console.log(resp)
-
+    }).then(async (resp) => {
         if (resp.status == 200) return window.location.replace("/")
+
+        await resp.json().then((data) => {
+            let errors = data.errors
+            if (!errors) return // TODO: Show toast message - Something went wrong
+
+            document.getElementById("login-error")!.innerText = errors.__all__ ? `- ${errors.__all__[0].message}` : "*"
+            document.getElementById("password-error")!.innerText = errors.__all__ ? `- ${errors.__all__[0].message}` : "*"
+        })
     }).catch(() => {
 
     })
