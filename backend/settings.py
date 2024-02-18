cd97c768f47687a43c29a4d44b43e95a1b0da5b3
Bartek <bmroczkowski@icloud.com>
2024-02-18 11:15:47 +0100

    Fix problems with langagu - middleware no longer needed

--- backend/settings.py
+++ backend/settings.py
@@ -52,8 +52,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
-    "lang.middleware.ActivateLanguage"
 ]
 
 ROOT_URLCONF = "backend.urls"
---
+++ lang/README.md
@@ -0,0 +1,8 @@
+# Langauge Options 
+Put any langauge configuration in this folder.
+
+## Commands
+```bash
+python manage.py makemessages -l <language_code>
+python manage.py compilemessages
+```
