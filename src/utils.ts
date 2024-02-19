2db10f7ce3d5c485465231c36d92ba01862d4908
Bartek <bmroczkowski@icloud.com>
2024-02-19 17:50:06 +0100

    Add theme menu, manage and save current theme. Prepare scripts for chat application

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,31 @@
 import * as $ from "jquery"
 
-function toggleContrast() {
-    let theme: "dark" | "light" | "high-contrast"
-    const currentTheme = document.documentElement.getAttribute("data-bs-theme")
+function preferredTheme() {
+    if (localStorage.getItem("theme") != "auto") return
 
-    if (currentTheme == "dark") theme = "light"
-    else if (currentTheme == "light") theme = "high-contrast"
-    else theme = "dark"
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) document.documentElement.setAttribute("data-bs-theme", "dark")
+    else document.documentElement.setAttribute("data-bs-theme", "light")
+}
 
+function changeTheme(theme?: string) {
+    if (!theme) {
+        let storedTheme = localStorage.getItem("theme")
+        if (storedTheme) return changeTheme(storedTheme)
+
+        return localStorage.setItem("theme", "auto")
+    }
+
+    if (theme == "auto") {
+        localStorage.setItem("theme", theme)
+        preferredTheme()
+
+        let theme_match = window.matchMedia("(prefers-color-scheme: dark)")
+        return theme_match.addEventListener("change", preferredTheme)
+    } else if (theme != "dark" && theme != "light" && theme != "high-contrast") return
+
+    localStorage.setItem("theme", theme)
     document.documentElement.setAttribute("data-bs-theme", theme)
 }
 
-$("#toggleTheme").on("click", toggleContrast)
+$(".change-theme").each((_, el) => { $(el).on("click", () => changeTheme(el.id)) })
+changeTheme()
