4afe65e214be76aaf2aa7d26c53bc3d5176dc45e
Bartek <bmroczkowski@icloud.com>
2024-02-20 16:27:44 +0100

    Imporve search form behavior. Fix lastUsername check

--- src/app/channels.ts
+++ src/app/channels.ts
@@ -27,14 +27,16 @@ import { showToast } from "../utils"
 //     })
 // })
 
-let lastUsername: string
 
-$(".search-form").each((_, form) => {
-    $(form).on("submit", async (e) => {
+$(".search-form").each((_, el) => {
+    const form = $(el)
+    let lastUsername: string
+
+    form.on("submit", async (e) => {
         e.preventDefault()
-        let formType = form.id == "direct" ? "direct" : "group"
 
-        let username = $(`#search-inpt-${formType}`).val()
+        let username = form.find("#search-inpt").val()
+        console.log(username)
         if (!username || username == lastUsername) return
 
         lastUsername = (username as string)
@@ -51,16 +53,16 @@ $(".search-form").each((_, form) => {
         }).then(async (resp) => {
             await resp.json().then((data) => {
                 if (resp.status == 200 && data.user) {
-                    $(`#searched-users-${formType}`).html(`
-                    <button class="channel-open btn d-flex align-items-center w-100">
-                        <img class="sidebar-icon" src="api/files/${data.user.avatar}" alt="Avatar">
-                        ${data.user.first_name} ${data.user.last_name}
-                    </button>
-                `)
+                    form.find("#searched-users").html(`
+                        <button class="channel-open btn d-flex align-items-center w-100" type="button">
+                            <img class="sidebar-icon" src="api/files/${data.user.avatar}" alt="Avatar">
+                            ${data.user.first_name} ${data.user.last_name}
+                        </button>
+                    `)
                     return
                 }
 
-                $(`#searched-users-${formType}`).html("")
+                form.find("#searched-users").html("")
 
                 let errors = data.errors
                 if (!errors) return showToast("API", "Coś poszło nie tak", "error")
