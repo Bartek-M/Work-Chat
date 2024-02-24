41713b0d1a509a7ae7a5c6427a4ded91080a6922
Igor <130784433+xigor1337@users.noreply.github.com>
2024-02-24 22:51:33 +0100

    writing instructions

--- frontend/views.py
+++ frontend/views.py
@@ -26,6 +26,11 @@ view_register = lambda request: app(request, "register.html")
 docs_main = lambda request: render(request, "docs/main.html")
 docs_install = lambda request: render(request, "docs/install.html")
 docs_manual = lambda request: render(request, "docs/manual.html")
+docs_backups = lambda request: render(request, "docs/backups.html")
+docs_functions = lambda request: render(request, "docs/functions.html")
+docs_scalability = lambda request: render(request, "docs/scalability.html")
+docs_security = lambda request: render(request, "docs/security.html")
+docs_performence = lambda request: render(request, "docs/performence.html")
 
 def view_logout(request):
     logout(request)
@@ -40,4 +45,9 @@ urlpatterns = [
     path("docs/", docs_main),
     path("docs/install/", docs_install),
     path("docs/manual/", docs_manual),
+    path("docs/backups/", docs_backups),
+    path("docs/functions/", docs_functions),
+    path("docs/scalability/", docs_scalability),
+    path("docs/security/", docs_security),
+    path("docs/performence/", docs_performence),
 ]
